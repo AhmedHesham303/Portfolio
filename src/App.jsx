53311b6b26e3c41b284ed3e248fae3c2e2ea271b
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-26 19:54:36 +0300

    building the app router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
