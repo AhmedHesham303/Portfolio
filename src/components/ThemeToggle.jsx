aa08c0e25442eca2f2ed5d6040aac7938ef2f9d8
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-27 12:38:51 +0300

    positining dark-mode-btn

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Sun, Moon } from "lucide-react";
+import { cn } from "../lib/utils";
 function ThemeToggle() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
@@ -28,11 +29,17 @@ function ThemeToggle() {
   }, []);
 
   return (
-    <button>
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-hidden"
+      )}
+    >
       {isDarkMode ? (
-        <Sun className="h-6 w-6 text-yellow-300 " onClick={toggleTheme} />
+        <Sun className="h-6 w-6 text-yellow-300 " />
       ) : (
-        <Moon className="h-6 w-6 text-blue-900" onClick={toggleTheme} />
+        <Moon className="h-6 w-6 text-blue-900" />
       )}
     </button>
   );
